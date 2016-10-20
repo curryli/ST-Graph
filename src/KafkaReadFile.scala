384a2784e1a1f87ed307ec278c7f1d1048543756
curryli <xurui.lee@msn.com>
2016-10-20 11:02:42 +0800

    can read file to kafka producer

--- src/KafkaReadFile.scala
+++ src/KafkaReadFile.scala
@@ -9,54 +9,32 @@ import scala.reflect.io.Path
 /**
   * Kafka Producer
   */
-class KafkaProduceMsg(brokerList : String, topic : String) extends Runnable{
+class KafkaProduceMsg() extends Runnable{
 
-  private val BROKER_LIST = brokerList //"master:9092,worker1:9092,worker2:9092"
-  private val TARGET_TOPIC = topic //"new"
-  private val DIR = "/usr/tmp"
-
-  /**
-    * 1、配置属性
-    * metadata.broker.list : kafka集群的broker，只需指定2个即可
-    * serializer.class : 如何序列化发送消息
-    * request.required.acks : 1代表需要broker接收到消息后acknowledgment,默认是0
-    * producer.type : 默认就是同步sync
-    */
   private val props = new Properties()
-  props.put("metadata.broker.list", this.BROKER_LIST)
-  props.put("serializer.class", "kafka.serializer.StringEncoder")
-  props.put("request.required.acks", "1")
-  props.put("producer.type", "async")
+  props.put("metadata.broker.list", "bB0104009" + ":" + "9092");
+  props.put("serializer.class", "kafka.serializer.StringEncoder");
 
-  /**
-    * 2、创建Producer
-    */
+
   private val config = new ProducerConfig(this.props)
   private val producer = new Producer[String, String](this.config)
 
-  /**
-    * 3、产生并发送消息
-    * 搜索目录dir下的所有包含“transaction”的文件并将每行的记录以消息的形式发送到kafka
-    *
-    */
+  
   def run() : Unit = {
     while(true){
-      val files = Path(this.DIR).walkFilter(p => p.isFile && p.name.contains("transaction"))
+      val file = "/home/hdanaly/xrli/graphx/GStream/transaction"
 
       try{
-        for(file <- files){
+        
           val reader = Source.fromFile(file.toString(), "UTF-8")
 
           for(line <- reader.getLines()){
-            val message = new KeyedMessage[String, String](this.TARGET_TOPIC, line)
+            val message = new KeyedMessage[String, String]("testlxr", null, line);
+            println(line);
             producer.send(message)
           }
 
-          //produce完成后，将文件copy到另一个目录，之后delete
-//          val fileName = file.toFile.name
-//          file.toFile.copyTo(Path("/root/Documents/completed/" +fileName + ".completed"))
-//          file.delete()
-        }
+  
       }catch{
         case e : Exception => println(e)
       }
@@ -77,12 +55,6 @@ class KafkaProduceMsg(brokerList : String, topic : String) extends Runnable{
 
 object ProduceMsg {
     def main(args : Array[String]): Unit ={
-      if(args.length < 2){
-        println("Usage : ProduceMsg master:9092,worker1:9092 new")
-
-        System.exit(1)
-      }
-
-      new Thread(new KafkaProduceMsg(args(0),args(1))).start()
+      new Thread(new KafkaProduceMsg()).start()
     }
 }
